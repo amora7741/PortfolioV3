ca327e051e74a0254ae28065eefd502de981e422
amora7741 <mora7741@gmail.com>
2024-10-31 11:40:52 -0700

    Setup page flexing on smaller screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import MouseHoverWrapper from "@/components/MouseHoverWrapper";
 export default function Home() {
   return (
     <MouseHoverWrapper>
-      <main className="relative mx-auto flex max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <main className="relative mx-auto flex max-w-screen-xl flex-col gap-20 px-6 py-12 md:px-12 md:py-20 lg:flex-row lg:px-16 lg:py-0">
         <Header />
         <MainContent />
       </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import SocialIcons from "./SocialIcons";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex h-screen flex-col justify-between lg:w-1/2 lg:py-24">
+    <header className="sticky flex min-w-fit flex-col justify-between gap-3 lg:top-0 lg:h-screen lg:w-1/2 lg:py-24">
       <div className="flex flex-col gap-3">
-        <h1 className="text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl">
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
           Alejandro Mora-Lopez
         </h1>
         <h2 className="text-xl font-medium">Full-Stack Software Engineer</h2>
         <p className="max-w-xs">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
-        <nav className="mt-16">
+        <nav className="mt-16 hidden lg:block">
           <ul>
             <li>Test</li>
             <li>Test</li>

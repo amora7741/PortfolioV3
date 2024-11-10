0c535f791bd84ff201d33ff44382e50473e27008
amora7741 <mora7741@gmail.com>
2024-11-09 16:31:11 -0800

    Add navbar hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,11 +22,14 @@ const NavbarItem = ({
 }) => (
   <li>
     <Link
-      className={`p-4 ${activeSection === href ? "text-black" : "text-black/30"}`}
+      className={`group flex items-center gap-4 p-4 ${activeSection === href ? "text-black" : "text-black/30"}`}
       href={`#${href}`}
       onClick={() => setActiveSection(href)}
     >
-      {name}
+      <span
+        className={`h-0.5 w-10 bg-black/30 group-hover:w-14 group-hover:bg-black ${activeSection === href ? "w-14 bg-black" : ""}`}
+      />
+      <p className="group-hover:text-black">{name}</p>
     </Link>
   </li>
 );
@@ -36,7 +39,7 @@ const Navbar = () => {
 
   return (
     <nav className="mt-16 hidden xl:block">
-      <ul className="-mx-4 space-y-6 text-xs font-bold uppercase tracking-widest">
+      <ul className="-mx-4 text-xs font-bold uppercase tracking-widest">
         {NavbarItems.map((item, i) => (
           <NavbarItem
             key={i}

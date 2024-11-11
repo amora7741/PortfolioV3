0c1802076643c6950ecebb87d681bd1cf1b7b6fa
amora7741 <mora7741@gmail.com>
2024-11-11 14:51:19 -0800

    Fix scroll bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import AboutMe from "@/components/AboutMe";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Header from "@/components/Header";
-import MouseHoverWrapper from "@/components/MouseHoverWrapper";
+import MouseCursor from "@/components/MouseCursor";
 
 export default function Home() {
   return (
-    <MouseHoverWrapper>
+    <>
+      <MouseCursor />
       <div className="relative mx-auto flex max-w-screen-xl flex-col gap-20 px-6 py-12 md:px-12 md:py-20 xl:flex-row xl:px-16 xl:py-0">
         <Header />
         <main className="relative h-full space-y-16 *:scroll-m-24 sm:space-y-24 xl:w-1/2 xl:space-y-32 xl:py-24">
@@ -15,6 +16,6 @@ export default function Home() {
           <Projects />
         </main>
       </div>
-    </MouseHoverWrapper>
+    </>
   );
 }
---
+++ src/components/MouseCursor.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const MouseCursor = () => {
+  const [position, setPosition] = useState({ x: window.innerWidth, y: 0 });
+
+  const updateMousePosition = (e: MouseEvent) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div
+      className="mouse-cursor pointer-events-none fixed -z-50 size-[60rem] rounded-full"
+      style={{
+        left: position.x,
+        top: position.y,
+        transform: "translate(-50%, -50%)",
+        background:
+          "radial-gradient(circle, rgba(59, 130, 246, 0.1) 0%, rgba(59, 130, 246, 0) 70%)",
+      }}
+    />
+  );
+};
+
+export default MouseCursor;

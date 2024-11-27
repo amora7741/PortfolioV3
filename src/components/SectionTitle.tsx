a9f1c809ad42407fb7f193879a0f13162bdacd08
amora7741 <mora7741@gmail.com>
2024-11-27 13:18:42 -0800

    Revert section title changes

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,5 @@
 const SectionTitle = ({ children }: { children: React.ReactNode }) => (
-  <h1 className="sticky top-0 z-50 -mx-6 -my-4 w-screen bg-background/50 p-4 px-6 font-bold uppercase backdrop-blur-xl md:-mx-12 md:px-12 xl:hidden">
+  <h1 className="sticky top-0 z-50 -mx-6 -my-4 w-screen bg-background/5 p-4 px-6 font-bold uppercase backdrop-blur-md md:-mx-12 md:px-12 xl:hidden">
     {children}
   </h1>
 );
